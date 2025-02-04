6d2c78deba53ae8de9a44b8c1314bb79f884b077
Michail Kanakas <michail.kanakas@studentfr.ch>
2025-02-04 16:39:05 +0100

    update

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,7 +1,18 @@
 package app;
 
 public class Application {
-    
+
+        public static void main(String[] args) {
+                Personne p1 = new Personne("Michel", "JSP");
+                Personne p2 = new Personne("Micro", "Penis");
+                Personne p3 = new Personne("Eland", "Osman");
+        
+                System.out.println(p1);    
+                System.out.println(p2);
+                System.out.println(p3);    
+        }
+
+       
         //
         //              )        (                 ) (           (         (
         //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
